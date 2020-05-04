ff00b73cc0303f19a332193540a6c8f973042141
Brian Schunck <bgschunck@gmail.com>
2020-05-03 19:32:33 -0700

    Refactored the script for making anagram tables into a class.

--- maketable.py
+++ maketable.py
@@ -3,80 +3,98 @@
 # Script to build a table for fast lookup of anagrams
 #
 # TODO: Make a class for the anagram table maker with the table as a member variable
+#
+# TODO: Add support for JSON format
 
 from nltk.corpus import words
 import yaml
+import json
+import os
 
 
-# Define global for the anagram dictionary
-anagram_dict = {}
+class AnagramTable():
+    """Table of anagrams supporting fast lookup to quickly determine whether a word is an anagram."""
 
+    def __init__(self):
+        self.anagram_dict = {}
 
-def canonical(string):
-    """Return the string in canonical format: Lowercase without leading or trailing whitespace."""
-    return string.strip().lower()
+    @staticmethod
+    def canonical(string):
+        """Return the string in canonical format: Lowercase without leading or trailing whitespace."""
+        return string.strip().lower()
 
+    @staticmethod
+    def stringify(list):
+        """Convert a list of characters into a canonical string."""
+        return AnagramTable.canonical(''.join(list))
 
-def stringify(list):
-    """Convert a list of characters into a canonical string."""
-    return canonical(''.join(list))
+    @staticmethod
+    def anagram_key(word):
+        """Return the anagram dictionary key for the specified word."""
+        return AnagramTable.stringify(sorted(word))
 
+    def make_anagram_table(self, limit=0):
+        """Make a table of anagrams indexed by the anagram key."""
 
-def anagram_key(word):
-    """Return the anagram dictionary key for the specified word."""
-    return stringify(sorted(word))
+        self.anagram_dict.clear()
 
+        for word in words.words():
 
-def make_anagram_table(limit=0):
-    """Make a table of anagrams indexed by the anagram key."""
+            # Terminate if the dictionary limit has been reached
+            if limit > 0 and len(anagram_dict) >= limit: break
 
-    for word in words.words():
+            # Anagram keys and words are lowercase
+            word = self.canonical(word)
 
-        # Terminate if the dictionary limit has been reached
-        if limit > 0 and len(anagram_dict) >= limit: break
+            key = self.anagram_key(word)
+            if key in self.anagram_dict:
+                # Is this word already in the anagram table?
+                if not word in self.anagram_dict[key]:
+                    # Append the new word onto the list of words with this key
+                    self.anagram_dict[key].append(word)
+            else:
+                # Create a new list in the anagram dictionary for this key
+                self.anagram_dict[key] = [word]
 
-        # Anagram keys and words are lowercase
-        word = canonical(word)
+    def print_anagram_table(self):
+        """Print the anagram table."""
 
-        key = anagram_key(word)
-        if key in anagram_dict:
-            # Is this word already in the anagram table?
-            if not word in anagram_dict[key]:
-                # Append the new word onto the list of words with this key
-                anagram_dict[key].append(word)
-        else:
-            # Create a new list in the anagram dictionary for this key
-            anagram_dict[key] = [word]
+        for key, word_list in self.anagram_dict.items():
+            print(key, word_list)
 
+    def save_anagram_table(self, pathname):
+        """Save the anagram table to the specified file."""
 
-def print_anagram_table():
-    """Print the anagram table."""
+        filetype = os.path.splitext(pathname)[1][1:].lower()
 
-    for key, word_list in anagram_dict.items():
-        print(key, word_list)
+        with open(pathname, 'w') as output:
+            if filetype == 'yaml':
+                output.write(yaml.dump(self.anagram_dict))
+            elif filetype == 'json':
+                output.write(json.dumps(self.anagram_dict, indent=2) + '\n')
+            else:
+                exit("Unknown filetype: %s" % filetype)
 
+    def load_anagram_table(self, pathname):
+        """Load the anagram table from the specified file."""
 
-def save_anagram_table(anagram_table, pathname):
-    """Save the anagram table to the specified file."""
+        filetype = os.path.splitext(pathname)[1][1:].lower()
 
-    with open(pathname, 'w') as output:
-        output.write(yaml.dump(anagram_table))
-
-
-def load_anagram_table(pathname):
-    """Load the anagram table from the specified file."""
-
-    with open(pathname) as input:
-        return yaml.load(input, Loader=yaml.FullLoader)
-
+        with open(pathname) as input:
+            if filetype == 'yaml':
+                self.anagram_dict = yaml.load(input, Loader=yaml.FullLoader)
+            elif filetype == 'json':
+                self.anagram_dict = json.load(input)
+            else:
+                exit("Unknown filetype: %s" % filetype)
 
 if __name__ == "__main__":
 
-    # Start with an enpty anagram table
-    anagram_dict.clear()
+    # Create an anagram table
+    anagram_table = AnagramTable()
 
     # Make the anagram table with no limit in the number of entries
-    make_anagram_table()
+    anagram_table.make_anagram_table()
 
     # Save the anagram table
-    save_anagram_table(anagram_dict, "anagram.yaml")
+    anagram_table.save_anagram_table("anagram.json")
